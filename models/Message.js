f1571b18553988fb99a6382b98918b53131cb0b8
Saurav <114897038+Saurav-5123@users.noreply.github.com>
2025-06-27 13:24:54 +0530

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -1,18 +1,13 @@
 const mongoose = require("mongoose");
 
 const messageSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
+  name: String,
+  email: String,
+  message: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
   },
-  email: {
-    type: String,
-    required: true
-  },
-  message: {
-    type: String,
-    required: true
-  }
 });
 
 module.exports = mongoose.model("Message", messageSchema);
